d8935c0123a1376552e9545fb405cad1a42dea87
devindianushika <devindianushika@gmail.com>
2020-05-17 22:57:15 +0530

    added  hosting part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ActivitiesComponent } from './components/activities/activities.component';
+
 
 
 const routes: Routes = [
 
   // { path: 'nav', component: NavbarComponent},
-  { path: 'dash',      component: DashboardComponent },
-  { path: 'act',      component: ActivitiesComponent },
-
+ 
 
 
 ];
--- src/app/directives/setmycolor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SetmycolorDirective } from './setmycolor.directive';
-
-describe('SetmycolorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SetmycolorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
